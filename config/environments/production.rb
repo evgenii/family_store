0825c05e2470ddccb266a35e7cb98fbdbd389aa9
Evgenii.S.Semenchuk <evgenii.s.semenchuk@gmail.com>
2017-10-29 01:08:25 +0300

    more config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :error   # :debug, :info, :warn, :error, and :fatal
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
